07a89eac3dbaffe95a57f6475fdb24bc76eb7c06
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-11-22 17:05:32 +0100

    ADD  Express Blog - Middlewares

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express()
 const port = 3000
 const postsRouter = require('./routers/posts')
 const posts = require('./data/posts')
+const errorsHandler = require('./middlewares/errorsHandler')
+const notFound = require('./middlewares/notFound')
 
 
-app.use(express.json())
 app.use(express.static('public'))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
-	res.send('<h1>express-blog-intro</h1>');
+	// res.send('<h1>express-blog-intro</h1>');
+    throw new error('non vabbene')
 })
 
 app.get('/bacheca', (req, res) => {
@@ -21,6 +25,10 @@ app.get('/bacheca', (req, res) => {
 })
 
 app.use('/posts', postsRouter)
+
+
+app.use(errorsHandler)
+app.use(notFound)
 
 app.listen(port, () => {
 	console.log(`Server listening on port: ${port}`)
